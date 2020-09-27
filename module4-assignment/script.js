8fe013e030f40bb666dc13f2e89de14406f989bd
vidya1710 <69502392+vidya1710@users.noreply.github.com>
2020-09-27 21:34:31 +0530

    Update script.js

--- module4-assignment/script.js
+++ module4-assignment/script.js
@@ -27,20 +27,14 @@ assignment.
 WARNING!!! WARNING!!!
 */
 
-
-(function () {
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
