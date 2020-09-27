1d154356733f6c53c106df8315818ea9b7a47d72
vidya1710 <69502392+vidya1710@users.noreply.github.com>
2020-09-27 21:31:21 +0530

    Update SpeakerGoodBye.js

--- module4-assignment/SpeakerGoodBye.js
+++ module4-assignment/SpeakerGoodBye.js
@@ -24,11 +24,10 @@
 
 
 (function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
 })(window);
