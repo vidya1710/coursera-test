94ddf15def9eefa2b0b6357a200e7fd90a90cd44
vidya1710 <69502392+vidya1710@users.noreply.github.com>
2020-09-27 21:33:05 +0530

    Update SpeakHello.js

--- module4-assignment/SpeakHello.js
+++ module4-assignment/SpeakHello.js
@@ -20,13 +20,11 @@
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
 
-
 (function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
 })(window);
